85126a790668a8a0ff14fe0ff2f477ed85ec8e9c
vldmitrofanov <shirker2006@gmail.com>
2015-03-13 03:51:04 +0800

    Create config.vici_sync.php

---
+++ config.vici_sync.php
@@ -0,0 +1,21 @@
+<?php
+
+$db1_host='192.168.11.11';
+$db1_user='root';
+$db1_pass='';
+$db1_db='status';
+
+$vici_host='38.102.228.45';
+$vici_user='root';
+$vici_pass='bigfoot123';
+$vici_db='asterisk';
+$vici_user_table = 'vicidial_users';
+
+$default_user_level = '4';
+
+// these users should not be removed or changed
+$vici_users = array (
+    0 => 'VDAD',
+    1 => 'VDCL',
+    2 => 'vladimirm',
+    3 => 'admin');
